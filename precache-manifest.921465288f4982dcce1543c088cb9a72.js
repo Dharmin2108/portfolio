933904884e4d5f33b46f32d2517713bcbd5b97fe
Dharmin Patel <dharminpatel99@gmail.com>
2020-07-30 01:11:11 +0530

    added build

---
+++ precache-manifest.921465288f4982dcce1543c088cb9a72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34cc4ff03c1e3139a9b0f35df457bd5",
+    "url": "/Dharmin2108.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "e40759efefa4e5e1f7ac",
+    "url": "/Dharmin2108.github.io/portfolio/static/css/main.60eec24a.chunk.css"
+  },
+  {
+    "revision": "218171fbb76f7c458316",
+    "url": "/Dharmin2108.github.io/portfolio/static/js/2.0dc2b175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dharmin2108.github.io/portfolio/static/js/2.0dc2b175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40759efefa4e5e1f7ac",
+    "url": "/Dharmin2108.github.io/portfolio/static/js/main.de22a0f7.chunk.js"
+  },
+  {
+    "revision": "e73138a81e078e51d073",
+    "url": "/Dharmin2108.github.io/portfolio/static/js/runtime-main.7f23a1d1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "24c4960e68770b79d2f61665a28c1dcc",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/fth.24c4960e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Dharmin2108.github.io/portfolio/static/media/skill.34409398.svg"
+  }
+]);
